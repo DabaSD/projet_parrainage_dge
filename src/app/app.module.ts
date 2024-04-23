66269ef5b91e1e9cbb4177fa5252188e206925cd
DabaSD <dabasambdiagne30@gmail.com>
2024-04-23 01:57:16 +0000

    Les routes marchent maintenant

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router'; // Importez RouterModule et Routes depuis @angular/router
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // Importez FormsModule et ReactiveFormsModule depuis @angular/forms
@@ -12,7 +12,7 @@ import { SearchCandidatComponent } from './search-candidat/search-candidat.compo
 import { ListeCandidatComponent } from './liste-candidat/liste-candidat.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProfilUserComponent } from './profil-user/profil-user.component';
-
+import { HttpClientModule } from '@angular/common/http';
 const routes: Routes = [
     { path: '', component: AppComponent },
     { path: 'connexion', component: LoginPageComponent },
@@ -41,9 +41,11 @@ const routes: Routes = [
   imports: [
     BrowserModule,
     RouterModule.forRoot(routes),
+    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
